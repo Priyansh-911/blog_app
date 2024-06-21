cd8ad4d70f97c0af4750c2d041729f38db9ba8e4
Priyansh-911 <priyansh91103@gmail.com>
2024-06-21 12:23:41 +0530

    Implemented Pagination in the GET api/blogs/ for fetching the blogs

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -115,12 +115,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'PAGE_SIZE': 5,
 }
 
 SIMPLE_JWT = {
--- website/views.py
+++ website/views.py
@@ -1,8 +1,14 @@
 from rest_framework import status, permissions
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Blog
-from .serializer import BlogSerializer
+from .serializer import BlogSerializer, LikeSerializer
+
+
+class BlogPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
 
 
 class BlogViewSet(APIView):
@@ -10,6 +16,14 @@ class BlogViewSet(APIView):
 
     def get(self, request):
         blogs = Blog.objects.all().order_by('-createdAt')
+        paginator = BlogPagination()
+        page = paginator.paginate_queryset(blogs, request)
+
+        if page is not None:
+            serializer = BlogSerializer(blogs, many=True)
+            print(paginator.get_page_size(request))
+            return paginator.get_paginated_response(serializer.data)
+
         serializer = BlogSerializer(blogs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -17,5 +31,13 @@ class BlogViewSet(APIView):
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({"msg": "Blog has been successfully created", "id": serializer.data['id']},
+                            status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LikeViewSet(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        serializer = LikeSerializer(data=request.data)
